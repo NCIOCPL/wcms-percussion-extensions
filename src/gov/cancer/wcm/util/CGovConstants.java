f023305fea507ab302b9709898092312c73727a6
holewr <webcommauto@users.noreply.github.com>
2010-07-28 18:21:50 +0000

    initial project setup

---
+++ src/gov/cancer/wcm/util/CGovConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package gov.cancer.wcm.util;
+
+/**
+ * @author whole
+ *
+ */
+public class CGovConstants {
+
+	 public static String DISPLAY_TITLE_FLD = "long_title";
+	 public static String FRIENDLY_URL_FLD = "";
+}
