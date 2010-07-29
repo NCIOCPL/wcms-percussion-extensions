8dbe467e96659961dc97c95b463074f8bca2ea32
holewr <webcommauto@users.noreply.github.com>
2010-07-29 14:06:07 +0000

    renamed

---
+++ extensions/src/gov/cancer/wcm/util/CGVConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package gov.cancer.wcm.util;
+
+/**
+ * @author whole
+ *
+ */
+public class CGVConstants {
+
+	 public static String DISPLAY_TITLE_FLD = "long_title";
+	 public static String FRIENDLY_URL_FLD = "pretty_url_name";
+}
