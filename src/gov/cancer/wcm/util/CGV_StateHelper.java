44799c8ac291ee1bf29e9cf4674b552182ae9ff2
wallsjt <webcommauto@users.noreply.github.com>
2010-08-06 16:07:18 +0000

    Added TODO's and javadocs JW

--- src/gov/cancer/wcm/util/CGV_StateHelper.java
+++ src/gov/cancer/wcm/util/CGV_StateHelper.java
@@ -1,43 +1,104 @@
 package gov.cancer.wcm.util;
 
+/**
+ * Wrapper class for the StateName enum.
+ * Provides the enum and functionality to transform it back
+ * and forth between enum values and string values.
+ * @author John Walls
+ *
+ */
 public class CGV_StateHelper {
 	
+	/**
+	 * Default Constructor
+	 */
 	public CGV_StateHelper(){
+		name = null;
+		state = null;
 	}
 	
+	//TODO: Eventually add a constructor and methods to allow stateid to be passed in.
+	
+	/**
+	 * Constructor that allows a string to be passed in.
+	 * That string will setup the name/statename enum object
+	 * for the CGV_StateHelper object.
+	 * @param stateName - the name of the state.
+	 */
 	public CGV_StateHelper(String stateName){
 		name = stateName;
 		state = toStateName(name);
 	}
 	
+	/**
+	 * Constructor that allows a StateName enum type to be passed
+	 * in, and the object (name/stateName) is build around that.
+	 * @param stateNameObj - the StateName enum object.
+	 */
 	public CGV_StateHelper(StateName stateNameObj){
 		state = stateNameObj;
 		name = toString(stateNameObj);
 	}
 	
+	/**
+	 * Gets the StateName enum object.
+	 * @return the StateName enum object
+	 */
 	public StateName getState() {
 		return state;
 	}
 
+	/**
+	 * Sets the StateName enum object.
+	 * @param state - the new StateName enum to set this.state to.
+	 */
 	public void setState(StateName state) {
 		this.state = state;
 	}
 
+	/**
+	 * Gets the name of the object.
+	 * @return the string form of the state name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the string form of the state name.
+	 * @param name - the string this.name is set to.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * The StateName enum object type for this CGV_StateHelper object.
+	 */
 	private StateName state;
+	
+	/**
+	 * The name of the object's state.
+	 */
 	private String name;
 
+	/**
+	 * Enum containing the different state names for the system.
+	 * @author John Walls
+	 *
+	 */
 	public enum StateName implements Comparable<StateName>
 	{DRAFT, REVIEW, PUBLIC, ARCHIVED, EDITING, REAPPROVAL;
 
-	//Returns; 0 if equal, -1 if left < right, 1 if left > right, 2 for a null compare.
+	/**
+	 * Compares two StateName objects and returns the operator that figures how they are related.
+	 * To call, create the Helper object, and pass in two new objects.
+	 * Exp: CGV_StateHelper( )  //TODO: Fix this, does not provide correct functionality from this space.
+	 * 
+	 * @param left - the left side of the compare (exp: left < right)
+	 * @param right - the right side of the compare (exp: left < right)
+	 * @return 0 if equal, -1 if left < right, 1 if left > right, 2 for a null compare.
+	 */
 	public static int compare(StateName left, StateName right){
 		if( (left == DRAFT) || (left == EDITING) || (left == REAPPROVAL) ){
 			if( (right == DRAFT) || (right == EDITING) || (right == REAPPROVAL) )
@@ -65,6 +126,11 @@ public class CGV_StateHelper {
 	}
 	}
 
+	/**
+	 * Returns the StateName enum for the string passed in, null if one DNE.
+	 * @param curr - the string to get the enum for.
+	 * @return the StateName enum type for that string, or null.
+	 */
 	public StateName toStateName(String curr){
 		if(curr == "Draft" )
 			return StateName.DRAFT;
@@ -82,6 +148,11 @@ public class CGV_StateHelper {
 			return null;
 	}
 
+	/**
+	 * Takes a StateName enum type and converts it to its string name.
+	 * @param curr - the StateName object
+	 * @return the string value of that enum.
+	 */
 	public String toString(StateName curr){
 		if(curr == StateName.DRAFT )
 			return "Draft";
@@ -99,6 +170,9 @@ public class CGV_StateHelper {
 			return "Null";
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString(){
 		return toString(state);
 	}
