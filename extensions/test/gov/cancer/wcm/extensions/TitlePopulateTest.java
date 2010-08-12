166138363208a506c1c20ba8875d110ace4e6f4b
wallsjt <webcommauto@users.noreply.github.com>
2010-08-12 14:39:32 +0000

    added some new tests

--- extensions/test/gov/cancer/wcm/extensions/TitlePopulateTest.java
+++ extensions/test/gov/cancer/wcm/extensions/TitlePopulateTest.java
@@ -2,6 +2,8 @@ package test.gov.cancer.wcm.extensions;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import java.lang.reflect.Method;
 
 import gov.cancer.wcm.extensions.CGV_TitlePopulate;
@@ -13,8 +15,19 @@ public class TitlePopulateTest {
 		Method p = CGV_TitlePopulate.class.getDeclaredMethod("modifyTitle",
 				new Class[]{String.class});
 		p.setAccessible(true);
-		String title = "Test Title";
-		String sysTitle =(String)p.invoke(null, new Object[]{title});
+		String title = "Title Test";
+		String sysTitle = "test";
+		System.out.println(sysTitle);
+		sysTitle = (String)p.invoke(null, new Object[]{title});
 		assertEquals(sysTitle.length(), title.length() + 7);
+		System.out.println(sysTitle);
+		assertFalse(sysTitle.substring(0, 4) == "test" );
+		
+		String temp = sysTitle;
+		sysTitle = (String)p.invoke(null, new Object[]{title});
+		System.out.println(temp);
+		System.out.println(sysTitle);
+		assertEquals(sysTitle, temp);
+		
 	}
 }
