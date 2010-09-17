5e271e93e6cf59531a4ebd6fccabec77b7766d04
wallsjt <webcommauto@users.noreply.github.com>
2010-09-17 13:38:05 +0000

    Added new top level types. (cgvBooklet and cgvBookletPage)

--- src/gov/cancer/wcm/util/CGVConstants.java
+++ src/gov/cancer/wcm/util/CGVConstants.java
@@ -11,6 +11,6 @@ public class CGVConstants {
 
 	 public static String DISPLAY_TITLE_FLD = "long_title";
 	 public static String FRIENDLY_URL_FLD = "pretty_url_name";
-	 public static String[] TOP_CONTENT_TYPE_NAMES = {"nciGeneral","nciLandingPage","cgvTopicSearchCategory","cgvClinicalTrialResult","cgvDrugInfoSummary","cgvFactSheet","cgvFeaturedClinicalTrial","cgvGeneric","cgvHome","cgvInclude","cgvPressRelease"};
+	 public static String[] TOP_CONTENT_TYPE_NAMES = {"cgvBooklet", "cgvBookletPage", "nciGeneral","nciLandingPage","cgvTopicSearchCategory","cgvClinicalTrialResult","cgvDrugInfoSummary","cgvFactSheet","cgvFeaturedClinicalTrial","cgvGeneric","cgvHome","cgvInclude","cgvPressRelease"};
 //TODO: move TOP_CONTENT_TYPE_NAMES to the bean xml file for CGV_OnDemandPublishService
 }
