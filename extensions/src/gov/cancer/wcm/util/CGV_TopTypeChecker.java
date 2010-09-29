36253a9ff19a0150bdb3719f6fc8802042ba6b73
wallsjt <webcommauto@users.noreply.github.com>
2010-09-29 19:51:18 +0000

    Added the method to check multiple auto slot types against the config file.

--- extensions/src/gov/cancer/wcm/util/CGV_TopTypeChecker.java
+++ extensions/src/gov/cancer/wcm/util/CGV_TopTypeChecker.java
@@ -3,7 +3,11 @@
  */
 package gov.cancer.wcm.util;
 
+import gov.cancer.wcm.util.CGV_StateHelper.StateName;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import com.percussion.services.content.data.PSContentTypeSummary;
 import com.percussion.webservices.content.IPSContentWs;
@@ -40,51 +44,26 @@ public class CGV_TopTypeChecker {
 		}
 		return false;
 	}
-	
-	/**
-	 * Returns true if this contentTypeId is a promo url type
-	 * @param contentTypeId - id to check
-	 * @return true if in list
-	 */
-	public static boolean URLAutoSlotType(int contentTypeId, IPSContentWs cmgr) {
-		//get array of type names
-		String promoUrl = CGVConstants.PROMO_URL;
-		if (bDebug) System.out.print("DEBUG: checking promo url ");
-		//get all summaries matching the current type
-		List<PSContentTypeSummary> summaries = cmgr.loadContentTypes(promoUrl);
-		if (bDebug) System.out.println("the size of the content type summary list is " + summaries.size());
-		//get the first item
-		if(summaries.size() != 0 ){
-			PSContentTypeSummary summaryItem = summaries.get(0);
 
-			if (contentTypeId == summaryItem.getGuid().getUUID()) {
-				return true;
+	/**
+	 * Returns true if this contentTypeId is in the provided checkList of autoSlot types.
+	 * @param	intValue - the value of the content type id.
+	 * @param	cmgr - the content Web service
+	 * @param	checList - the list of auto Slot needed content types, generated from the onDemand xml config file.
+	 * @return a list of content id's that need to be published.  Never null, possibly empty (if no content itds are to be published).
+	 */
+	public static List<Integer> autoSlotChecker(int intValue, IPSContentWs cmgr, Map<String,List<String>> checkList) {
+		
+		List<Integer> returnThis = new ArrayList<Integer>();
+		if( checkList.containsKey(Integer.toString(intValue)) )
+		{
+			List<String> m = checkList.get(Integer.toString(intValue));
+			for( String s: m ){
+				returnThis.add(Integer.parseInt(s));
 			}
 		}
-		return false;
-	}
-	
-	/**
-	 * Returns true if this contentTypeId is a Cancer topic search category type
-	 * @param contentTypeId - id to check
-	 * @return true if in list
-	 */
-	public static boolean TopicSearchAutoSlotType(int contentTypeId, IPSContentWs cmgr) {
-		//get array of type names
-		String topicSearch = CGVConstants.TOPIC_SEARCH_CATEGORY;
-		if (bDebug) System.out.print("DEBUG: checking Topic Search category ");
-		//get all summaries matching the current type
-		List<PSContentTypeSummary> summaries = cmgr.loadContentTypes(topicSearch);
-		if (bDebug) System.out.println("the size of the content type summary list is " + summaries.size());
-		//get the first item
-		if(summaries.size() != 0 ){
-			PSContentTypeSummary summaryItem = summaries.get(0);
-
-			if (contentTypeId == summaryItem.getGuid().getUUID()) {
-				return true;
-			}
-		}
-		return false;
+		return returnThis;
+		
 	}
 	
 
