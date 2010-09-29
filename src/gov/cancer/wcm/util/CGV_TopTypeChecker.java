7eb5e45a0c9efed87e6da0cd415dbcbcdd15a489
wallsjt <webcommauto@users.noreply.github.com>
2010-09-29 14:51:22 +0000

    Added bools to check content type against the Promo Url, and the cancer topic search.  This is adding functionality to auto publish auto slot types.

--- src/gov/cancer/wcm/util/CGV_TopTypeChecker.java
+++ src/gov/cancer/wcm/util/CGV_TopTypeChecker.java
@@ -14,7 +14,7 @@ import com.percussion.webservices.content.IPSContentWs;
  *
  */
 public class CGV_TopTypeChecker {
-	private static boolean bDebug = true;
+	private static boolean bDebug = false;
 	
 	/**
 	 * Returns true if this contentTypeId is in the list of topmost content types
@@ -40,5 +40,52 @@ public class CGV_TopTypeChecker {
 		}
 		return false;
 	}
+	
+	/**
+	 * Returns true if this contentTypeId is a promo url type
+	 * @param contentTypeId - id to check
+	 * @return true if in list
+	 */
+	public static boolean URLAutoSlotType(int contentTypeId, IPSContentWs cmgr) {
+		//get array of type names
+		String promoUrl = CGVConstants.PROMO_URL;
+		if (bDebug) System.out.print("DEBUG: checking promo url ");
+		//get all summaries matching the current type
+		List<PSContentTypeSummary> summaries = cmgr.loadContentTypes(promoUrl);
+		if (bDebug) System.out.println("the size of the content type summary list is " + summaries.size());
+		//get the first item
+		if(summaries.size() != 0 ){
+			PSContentTypeSummary summaryItem = summaries.get(0);
+
+			if (contentTypeId == summaryItem.getGuid().getUUID()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns true if this contentTypeId is a Cancer topic search category type
+	 * @param contentTypeId - id to check
+	 * @return true if in list
+	 */
+	public static boolean TopicSearchAutoSlotType(int contentTypeId, IPSContentWs cmgr) {
+		//get array of type names
+		String topicSearch = CGVConstants.TOPIC_SEARCH_CATEGORY;
+		if (bDebug) System.out.print("DEBUG: checking Topic Search category ");
+		//get all summaries matching the current type
+		List<PSContentTypeSummary> summaries = cmgr.loadContentTypes(topicSearch);
+		if (bDebug) System.out.println("the size of the content type summary list is " + summaries.size());
+		//get the first item
+		if(summaries.size() != 0 ){
+			PSContentTypeSummary summaryItem = summaries.get(0);
+
+			if (contentTypeId == summaryItem.getGuid().getUUID()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 
 }
