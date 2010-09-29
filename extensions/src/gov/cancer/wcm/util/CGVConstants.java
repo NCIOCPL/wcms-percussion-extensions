1d461f15e54f6f1922cc4f8c9d37295f60831a2f
wallsjt <webcommauto@users.noreply.github.com>
2010-09-29 14:47:18 +0000

    Added Promo Url and Topic Search Category types, for use in the modified on Demand publisher for auto slot types.

--- extensions/src/gov/cancer/wcm/util/CGVConstants.java
+++ extensions/src/gov/cancer/wcm/util/CGVConstants.java
@@ -11,6 +11,8 @@ public class CGVConstants {
 
 	 public static String DISPLAY_TITLE_FLD = "long_title";
 	 public static String FRIENDLY_URL_FLD = "pretty_url_name";
+	 public static String PROMO_URL = "cgvPromoUrl";
+	 public static String TOPIC_SEARCH_CATEGORY = "cgvTopicSearchCategory";
 	 public static String[] TOP_CONTENT_TYPE_NAMES = {"cgvBooklet", "cgvContentSearch", "cgvBookletPage", "nciGeneral","nciLandingPage","cgvTopicSearchCategory","cgvClinicalTrialResult","cgvDrugInfoSummary","cgvFactSheet","cgvFeaturedClinicalTrial","cgvGeneric","cgvHome","cgvInclude","cgvPressRelease"};
 //TODO: move TOP_CONTENT_TYPE_NAMES to the bean xml file for CGV_OnDemandPublishService
 }
