9582af98f7fcd1c10821e98a62580b52a53f0429
wallsjt <webcommauto@users.noreply.github.com>
2010-10-06 22:10:40 +0000

    Added multi page lists, and organized the single page types list

--- src/gov/cancer/wcm/util/CGVConstants.java
+++ src/gov/cancer/wcm/util/CGVConstants.java
@@ -11,6 +11,25 @@ public class CGVConstants {
 
 	 public static String DISPLAY_TITLE_FLD = "long_title";
 	 public static String FRIENDLY_URL_FLD = "pretty_url_name";
-	 public static String[] TOP_CONTENT_TYPE_NAMES = {"cgvBooklet", "cgvContentSearch", "cgvBookletPage", "nciGeneral","nciLandingPage","cgvTopicSearchCategory","cgvClinicalTrialResult","cgvDrugInfoSummary","cgvFactSheet","cgvFeaturedClinicalTrial","cgvGeneric","cgvHome","cgvInclude","cgvPressRelease"};
+	 public static String[] MULTI_PAGE_CONTAINER = {"cgvBooklet", "cgvPowerPoint", "cgvCancerBulletin"};
+	 public static String[] MULTI_PAGE_PAGES = {"cgvBookletPage", "cgvPowerPointPage", "cgvCancerBulletinPage"};
+	 public static String[] TOP_CONTENT_TYPE_NAMES = {
+		 "cgvBooklet", 
+		 "cgvBookletPage", 
+		 "cgvCancerBulletin", 
+		 "cgvCancerBulletinPage", 
+		 "cgvClinicalTrialResult",
+		 "cgvContentSearch", 
+		 "cgvDrugInfoSummary",
+		 "cgvFactSheet",
+		 "cgvFeaturedClinicalTrial",
+		 "cgvPowerPoint",
+		 "cgvPowerPointPage",
+		 "cgvPressRelease",
+		 "cgvTopicSearchCategory",
+		 "nciGeneral",
+		 "nciHome",
+		 "nciLandingPage"
+		 };
 //TODO: move TOP_CONTENT_TYPE_NAMES to the bean xml file for CGV_OnDemandPublishService
 }
