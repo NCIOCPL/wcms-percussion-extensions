faa3634ffb229202e91537c2b8a7e8c617ee8114
wallsjt <webcommauto@users.noreply.github.com>
2010-10-13 18:09:24 +0000

    Copy the sys_title of a navon into the pretty url, if the url is null.

--- src/gov/cancer/wcm/extensions/CGV_NavonUrlPopulate.java
+++ src/gov/cancer/wcm/extensions/CGV_NavonUrlPopulate.java
@@ -40,47 +40,30 @@ public class CGV_NavonUrlPopulate extends PSDefaultExtension implements
 	public void preProcessRequest(Object[] params, IPSRequestContext request)
 			throws PSAuthorizationException, PSRequestValidationException,
 			PSParameterMismatchException, PSExtensionProcessingException {
-		String displaytitle = request.getParameter("sys_title");
-		//LOGGER.debug("******INVOKING titlePopulate");
-
-		Pattern q = Pattern.compile("[A-Za-z0-9\\-\\_\\.]*");
-		if(request.getParameter("sys_title") != null){
-			if(request.getParameter("pretty_url_folder_name") == null){
-				Matcher m = q.matcher(displaytitle);
-				if( !m.matches() ){
-
-					request.setParameter("pretty_url_folder_name", modifyTitle(displaytitle));
-				}
-				else{
-					request.setParameter("pretty_url_folder_name", displaytitle);
-				}
-			}
+		String systitle = request.getParameter("sys_title");
+		String pretty = request.getParameter("pretty_url_folder_name");
+		if(pretty.isEmpty() || pretty.length() == 0){
+				request.setParameter("pretty_url_folder_name", systitle);
 		}
+		
+//		String displaytitle = request.getParameter("sys_title");
+//		//LOGGER.debug("******INVOKING titlePopulate");
+//
+//		Pattern q = Pattern.compile("[A-Za-z0-9\\-\\_\\.]*");
+//		if(request.getParameter("sys_title") != null){
+//			if(request.getParameter("pretty_url_folder_name") == null){
+//				Matcher m = q.matcher(displaytitle);
+//				if( !m.matches() ){
+//
+//					request.setParameter("pretty_url_folder_name", modifyTitle(displaytitle));
+//				}
+//				else{
+//					request.setParameter("pretty_url_folder_name", displaytitle);
+//				}
+//			}
+//		}
 	}
 
-	/**
-	 * Add a random number to the end of the title
-	 * @param displayTitle the title to modify
-	 * @return String modified title
-	 */
-	private static String modifyTitle(String title) {
-		if(title != null){
-			if(title.length() >= 248 ){
-				title = title.substring(0, 247);
-			}
-		}
-		String regex = " ";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(title);
-        String output = matcher.replaceAll("");
-        
-		regex = "[^A-Za-z0-9\\-\\_\\.]";
-		pattern = Pattern.compile(regex);
-		matcher = pattern.matcher(output);
-        output = matcher.replaceAll("_");
-		return output;
-	}
-	
 	public boolean canModifyStyleSheet() {
 		// TODO Auto-generated method stub
 		return false;
@@ -92,11 +75,4 @@ public class CGV_NavonUrlPopulate extends PSDefaultExtension implements
 		}
 	}
 
-	private static int get4DigitRandomNumber() {
-		Random rand = new Random();
-		int max = 9999;
-		int min = 1000;
-		int randomNum = rand.nextInt(max - min + 1) + min;
-		return randomNum;
-	}
 }
