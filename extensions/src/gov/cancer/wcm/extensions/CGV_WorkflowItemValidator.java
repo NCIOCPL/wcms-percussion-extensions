5321199c2ec9b96c2ad25bee4ba43bfb9e9b07c6
Bryan Pizzillo <bryanpizzillo@users.noreply.github.com>
2010-12-07 15:53:06 +0000

    Moved the validation and transitioning logic out into its own class.

--- extensions/src/gov/cancer/wcm/extensions/CGV_WorkflowItemValidator.java
+++ extensions/src/gov/cancer/wcm/extensions/CGV_WorkflowItemValidator.java
@@ -45,9 +45,8 @@ public class CGV_WorkflowItemValidator extends PSOAbstractItemValidationExit {
 	private static CGV_RelationshipHandlerService relationshipHandlerService;
 	private static IPSContentMgr contentManagerService;
 	private static IPSGuidManager guidManagerService; 
-	private static PSOWorkflowInfoFinder workflowInfoFinder;
-	private static IPSCmsContentSummaries contentSummariesService;
-	private static WorkflowConfiguration workflowConfig;
+	private static PSOWorkflowInfoFinder workflowInfoFinder;	
+	
 	
 	/*
 	 * This initializes some of the different services
@@ -56,9 +55,7 @@ public class CGV_WorkflowItemValidator extends PSOAbstractItemValidationExit {
 		contentManagerService = PSContentMgrLocator.getContentMgr();
 		guidManagerService = PSGuidManagerLocator.getGuidMgr();
 		relationshipHandlerService = CGV_RelationshipHandlerServiceLocator.getCGV_RelatoinshipHandlerService();
-	    workflowInfoFinder = new PSOWorkflowInfoFinder();
-	    contentSummariesService = PSCmsContentSummariesLocator.getObjectManager();
-	    workflowConfig = WorkflowConfigurationLocator.getWorkflowConfiguration();
+	    workflowInfoFinder = new PSOWorkflowInfoFinder();	    
 	}
 	
 	/**
@@ -86,7 +83,8 @@ public class CGV_WorkflowItemValidator extends PSOAbstractItemValidationExit {
 	    if(super.matchDestinationState(contentid, transitionid, states))
 	    {
 	    	log.debug("Testing if transition of item is allowed, valid state for test");
-	    	performTest(req,errorDoc);
+	    	ContentItemWFValidatorAndTransitioner validator = new ContentItemWFValidatorAndTransitioner(log);
+	    	validator.performTest(req,errorDoc);
 	    } 
 	    setExclusive(req, false);
 	    }else {
@@ -94,76 +92,10 @@ public class CGV_WorkflowItemValidator extends PSOAbstractItemValidationExit {
 	    }	
 	}
 	
-	/**
-	 * Tests an item to see if the item should be allowed through the workflow.
-	 * If it is, and dependents which should transition with the item should
-	 * be transitioned through the workflow. 
-	 * @param request IPSRequestContext
-	 * @param errorDoc Document
-	 * @throws PSException
-	 */
 	public void performTest(IPSRequestContext request,Document errorDoc)
-		throws PSException {
-
-		log.debug("Workflow Item Validator: Performing Test...");
-
-		String transition = request.getParameter("sys_transitionid");
-		String currCID = request.getParameter("sys_contentid");
-		int id = Integer.parseInt(currCID);		
-			
-		//Get information about the content item this lets us get the GUID
-		//and allows us to get the content Type Name
-		PSComponentSummary contentItemSummary = contentSummariesService.loadComponentSummary(id);		 
-		String contentTypeName = CGV_TypeNames.getTypeName(contentItemSummary.getContentTypeId());
-		
-		log.debug("Initiating Push for Content Item: " + id + "(" + contentTypeName +")");
-		
-		/*
-		 * We need to check if:
-		 *  A) we are either
-		 *  	1. A top type - We must transition our children
-		 *  	2. A shared item - We are used multiple items
-		 *  	   and therefore should be transitioned separately.
-		 *  	   A shared item can still have children which will
-		 *         need to be transitioned.
-		 *      3. A component which must be moved on its own and
-		 *         does not follow any fancy rules.
-		 *  B) we are a component of a page 
-		 */
-
-		//We should really check if we are updating, first publish, or unpublishing content.
-		
-		// Step 1. Check if this is a top type and handle appropriately
-		ContentTypeConfig config = workflowConfig.getContentTypes().getContentTypeOrDefault(contentTypeName);
-		
-		if (config == null) {
-			log.error("Recieved a null content type config when validating an item.");
-			PSItemErrorDoc.addError(errorDoc, ERR_FIELD, ERR_FIELD_DISP, "Error getting workflow config for item with id {0}", new Object[]{id});
-		} else {
-			if (config.getIsTopType()) {
-				//This is a top type.
-				log.debug("Content Item : " + id + ", of type: " + config.getName() +  ", is a top type.");
-				pushTopType(request, errorDoc, contentItemSummary);
-			} else {
-				//This is not a top type				
-				
-			}
-		}
-		
-		// Step 2. 
-		
-		PSItemErrorDoc.addError(errorDoc, ERR_FIELD, ERR_FIELD_DISP, "Error getting related ids for item with id {0}", new Object[]{id});
-		throw new PSException("Stopping");
-	}
-	
-	/**
-	 * Validates a top type and transitions its children through the workflow 
-	 * @param request
-	 * @param errorDoc
-	 * @param contentItemSummary
-	 */
-	private void pushTopType(IPSRequestContext request,Document errorDoc, PSComponentSummary contentItemSummary) {
-		
+	throws PSException {
+    	ContentItemWFValidatorAndTransitioner validator = new ContentItemWFValidatorAndTransitioner(log);
+    	validator.performTest(request,errorDoc);
 	}
 	
 	 /**
