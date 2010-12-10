fc57befccd4d0d522468cc418e47d7468d87998c
wallsjt <webcommauto@users.noreply.github.com>
2010-12-10 14:35:55 +0000

    Added a page count jexl expression.

--- extensions/src/gov/cancer/wcm/extensions/jexl/CGV_AssemblyTools.java
+++ extensions/src/gov/cancer/wcm/extensions/jexl/CGV_AssemblyTools.java
@@ -1,7 +1,17 @@
 package gov.cancer.wcm.extensions.jexl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Value;
+import javax.jcr.query.InvalidQueryException;
+import javax.jcr.query.Query;
+import javax.jcr.query.QueryResult;
+import javax.jcr.query.Row;
+import javax.jcr.query.RowIterator;
 
 import org.apache.commons.lang.Validate;
 import org.apache.commons.logging.Log;
@@ -11,11 +21,15 @@ import com.percussion.extension.IPSJexlExpression;
 import com.percussion.extension.IPSJexlMethod;
 import com.percussion.extension.IPSJexlParam;
 import com.percussion.extension.PSJexlUtilBase;
+import com.percussion.pso.jexl.PSOQueryTools;
 import com.percussion.services.assembly.IPSAssemblyItem;
+import com.percussion.services.contentmgr.IPSContentMgr;
+import com.percussion.services.contentmgr.PSContentMgrLocator;
 
 public class CGV_AssemblyTools extends PSJexlUtilBase implements IPSJexlExpression{
 
 	private static Log log = LogFactory.getLog(CGV_AssemblyTools.class);
+	private static IPSContentMgr cmgr = null;
 	
 	public CGV_AssemblyTools() {
 		super();
@@ -40,6 +54,28 @@ public class CGV_AssemblyTools extends PSJexlUtilBase implements IPSJexlExpressi
 			path = path.substring(0, path.lastIndexOf("/"));
 				
 		return path;
+	}
+	
+	@IPSJexlMethod(description = "Returns the number of pages for a dynamic auto slot", params = {
+		@IPSJexlParam(name = "path", description = "The entire JSR query") })
+	public int pagerCount(String path)
+			throws RepositoryException 
+	{
+		if(cmgr == null)
+		{
+			cmgr = PSContentMgrLocator.getContentMgr();
+		}
+	
+		
+		
+		//Query q = cmgr.createQuery(path, Query.SQL);
+		PSOQueryTools pso = new PSOQueryTools();
+		//QueryResult r =  cmgr.executeQuery(q, -1, null, null);
+		//List<Map<String, Value>> eq = new ArrayList<Map<String, Value>>();
+		return pso.executeQuery(path, -1, null, null).size();
+		
+		
+
 	}
 
 }
