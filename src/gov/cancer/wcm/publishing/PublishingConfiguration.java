851f7ece628c377480197e6b431b1e57d01d8989
Dion D'aquino <daquinohd@users.noreply.github.com>
2016-05-11 15:27:20 +0000

    Merged Hermes branch into Trunk.

--- src/gov/cancer/wcm/publishing/PublishingConfiguration.java
+++ src/gov/cancer/wcm/publishing/PublishingConfiguration.java
@@ -1,5 +1,8 @@
 package gov.cancer.wcm.publishing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /** Configuration class for publishing.  This replaces the crufty (and unused?)
  * CGV_OnDemandPublishService-beans bean.
  * @author learnb
@@ -8,6 +11,7 @@ package gov.cancer.wcm.publishing;
 public class PublishingConfiguration {
 
 	private PublishingJobBlockerCollection publicationBlockers;
+	private List<String> needsNextTopTypeList = new ArrayList<String>();
 	
 	/**Retrieve the collection of publishing edition blocking rules.
 	 * @return A PublishingJobBlockerCollection object. Never null.
@@ -16,7 +20,27 @@ public class PublishingConfiguration {
 		return this.publicationBlockers;
 	}
 	
-	public PublishingConfiguration(PublishingJobBlockerCollection blockingEditions){
+	/**Retrieve whether or not a content type needs the next top type for publishing.
+     * @return a boolean.
+     */
+	public boolean getNeedsNextTopType (String contentTypeName) {
+		boolean needsNextTopType = false;
+		if(needsNextTopTypeList.contains(contentTypeName)) {
+			needsNextTopType = true;
+		}
+		return needsNextTopType;
+	}
+	 
+    /**Retrieve the list of content types that need next top type for publishing.
+     * @return a List<String> object.
+     */
+    public List<String> getNeedsNextTopTypeList() {
+        return this.needsNextTopTypeList;
+    }
+	
+	public PublishingConfiguration(PublishingJobBlockerCollection blockingEditions, List<String> needsNextTopTypes){
 		this.publicationBlockers = blockingEditions;
+		this.needsNextTopTypeList.addAll(needsNextTopTypes);
 	}
 }
+
