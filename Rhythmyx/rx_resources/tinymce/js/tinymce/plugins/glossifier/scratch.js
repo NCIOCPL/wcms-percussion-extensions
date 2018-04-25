11465dfbf5eadd1a40b090cbee049a68bbaef63f
daquinohd <daquinohd@mail.nih.gov>
2018-04-25 10:29:04 -0400

    [WCMSPERC-418] attempt to re-draw glossifier as a modal

---
+++ Rhythmyx/rx_resources/tinymce/js/tinymce/plugins/glossifier/scratch.js
@@ -0,0 +1,67 @@
+/**
+ * plugin.js
+ *
+ * Released under LGPL License.
+ * Copyright (c) 1999-2015 Ephox Corp. All rights reserved
+ *
+ * License: http://www.tinymce.com/license
+ * Contributing: http://www.tinymce.com/contributing
+ */
+
+/*global tinymce:true */
+
+tinymce.PluginManager.add('glossifier', function(editor) {
+	function showDialog() {
+		
+		// variables
+		var settings = editor.settings;
+		var allContent = editor.getContent();
+		console.log(allContent);
+		
+		// Open modal window and set submit behavior
+		var win = editor.windowManager.open({
+			title: "Glossifier",
+			body: {
+				type: 'textbox',
+				name: 'code',
+				multiline: true,
+				minWidth: editor.getParam("code_dialog_width", 600),
+				minHeight: editor.getParam("code_dialog_height", Math.min(tinymce.DOM.getViewPort().h - 200, 500)),
+				spellcheck: false,
+				style: 'direction: ltr; text-align: left'
+			},
+			onSubmit: function(e) {
+				// We get a lovely "Wrong document" error in IE 11 if we
+				// don't move the focus to the editor before creating an undo
+				// transation since it tries to make a bookmark for the current selection
+				editor.focus();
+
+				editor.undoManager.transact(function() {
+					editor.setContent(e.data.code);
+				});
+
+				editor.selection.setCursorLocation();
+				editor.nodeChanged();
+			}
+		});
+
+		// Gecko has a major performance issue with textarea
+		// contents so we need to set it when all reflows are done
+		win.find('#code').value(editor.getContent({source_view: true}));
+	}
+
+	editor.addCommand("openGlossifier", showDialog);
+
+	editor.addButton('glossifier', {
+		icon: 'glossifier" style="background-image:url(\'../rx_resources/tinymce/images/glossify.gif\');"',
+		title : 'Glossify',
+		cmd : 'openGlossifier'
+	});
+
+	editor.addMenuItem('glossifier', {
+		icon: 'glossifier" style="background-image:url(\'../rx_resources/tinymce/images/glossify.gif\');"',
+		text : 'Glossify',
+		cmd : 'openGlossifier',
+		context: 'tools'
+	});
+});
