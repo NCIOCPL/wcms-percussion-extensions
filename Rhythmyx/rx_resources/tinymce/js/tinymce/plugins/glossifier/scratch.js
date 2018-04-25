97ff463835cd91f950f046b927df6c044ee02566
daquinohd <daquinohd@mail.nih.gov>
2018-04-25 11:32:37 -0400

    [WCMSPERC-418] using 'preview' plugin as a starting point

--- Rhythmyx/rx_resources/tinymce/js/tinymce/plugins/glossifier/scratch.js
+++ Rhythmyx/rx_resources/tinymce/js/tinymce/plugins/glossifier/scratch.js
@@ -11,46 +11,82 @@
 /*global tinymce:true */
 
 tinymce.PluginManager.add('glossifier', function(editor) {
-	function showDialog() {
-		
-		// variables
-		var settings = editor.settings;
-		var allContent = editor.getContent();
-		console.log(allContent);
-		
-		// Open modal window and set submit behavior
-		var win = editor.windowManager.open({
-			title: "Glossifier",
-			body: {
-				type: 'textbox',
-				name: 'code',
-				multiline: true,
-				minWidth: editor.getParam("code_dialog_width", 600),
-				minHeight: editor.getParam("code_dialog_height", Math.min(tinymce.DOM.getViewPort().h - 200, 500)),
-				spellcheck: false,
-				style: 'direction: ltr; text-align: left'
-			},
-			onSubmit: function(e) {
-				// We get a lovely "Wrong document" error in IE 11 if we
-				// don't move the focus to the editor before creating an undo
-				// transation since it tries to make a bookmark for the current selection
-				editor.focus();
+	var settings = editor.settings, sandbox = !tinymce.Env.ie;
 
-				editor.undoManager.transact(function() {
-					editor.setContent(e.data.code);
+	editor.addCommand('openGlossifier', function() {
+		editor.windowManager.open({
+			title: 'glossifier',
+			width: parseInt(editor.getParam("plugin_preview_width", "650"), 10),
+			height: parseInt(editor.getParam("plugin_preview_height", "500"), 10),
+			html: '<iframe src="javascript:\'\'" frameborder="0"' + (sandbox ? ' sandbox="allow-scripts"' : '') + '></iframe>',
+			buttons: {
+				text: 'Close',
+				onclick: function() {
+					this.parent().parent().close();
+				}
+			},
+			onPostRender: function() {
+				var previewHtml, headHtml = '';
+
+				headHtml += '<base href="' + editor.documentBaseURI.getURI() + '">';
+
+				tinymce.each(editor.contentCSS, function(url) {
+					headHtml += '<link type="text/css" rel="stylesheet" href="' + editor.documentBaseURI.toAbsolute(url) + '">';
 				});
 
-				editor.selection.setCursorLocation();
-				editor.nodeChanged();
+				var bodyId = settings.body_id || 'tinymce';
+				if (bodyId.indexOf('=') != -1) {
+					bodyId = editor.getParam('body_id', '', 'hash');
+					bodyId = bodyId[editor.id] || bodyId;
+				}
+
+				var bodyClass = settings.body_class || '';
+				if (bodyClass.indexOf('=') != -1) {
+					bodyClass = editor.getParam('body_class', '', 'hash');
+					bodyClass = bodyClass[editor.id] || '';
+				}
+
+				var preventClicksOnLinksScript = (
+					'<script>' +
+						'document.addEventListener && document.addEventListener("click", function(e) {' +
+							'for (var elm = e.target; elm; elm = elm.parentNode) {' +
+								'if (elm.nodeName === "A") {' +
+									'e.preventDefault();' +
+								'}' +
+							'}' +
+						'}, false);' +
+					'</script> '
+				);
+
+				var dirAttr = editor.settings.directionality ? ' dir="' + editor.settings.directionality + '"' : '';
+
+				previewHtml = (
+					'<!DOCTYPE html>' +
+					'<html>' +
+					'<head>' +
+						headHtml +
+					'</head>' +
+					'<body id="' + bodyId + '" class="mce-content-body ' + bodyClass + '"' + dirAttr + '>' +
+						editor.getContent() +
+						preventClicksOnLinksScript +
+					'</body>' +
+					'</html>'
+				);
+
+				if (!sandbox) {
+					// IE 6-11 doesn't support data uris on iframes
+					// so I guess they will have to be less secure since we can't sandbox on those
+					// TODO: Use sandbox if future versions of IE supports iframes with data: uris.
+					var doc = this.getEl('body').firstChild.contentWindow.document;
+					doc.open();
+					doc.write(previewHtml);
+					doc.close();
+				} else {
+					this.getEl('body').firstChild.src = 'data:text/html;charset=utf-8,' + encodeURIComponent(previewHtml);
+				}
 			}
 		});
-
-		// Gecko has a major performance issue with textarea
-		// contents so we need to set it when all reflows are done
-		win.find('#code').value(editor.getContent({source_view: true}));
-	}
-
-	editor.addCommand("openGlossifier", showDialog);
+	});
 
 	editor.addButton('glossifier', {
 		icon: 'glossifier" style="background-image:url(\'../rx_resources/tinymce/images/glossify.gif\');"',
